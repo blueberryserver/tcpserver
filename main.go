ebba6b66c629ac72a96f6b3c1a6baede1b5ea8ec
noom <noom@nexon.co.kr>
2017-04-06 11:52:38 +0900

    delay connection time

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/blueberryserver/tcpserver/network"
 )
@@ -10,16 +11,19 @@ func main() {
 	fmt.Printf("server start\r\n")
 
 	// 서버 시작 리슨 요청
-	server := network.NewServer("tcp", "127.0.0.1:10011")
+	server := network.NewServer("tcp", ":20202")
 	err := server.Listen()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
+	// 10초간 대기
+	time.Sleep(10 * time.Second)
+
 	// 클라이언트 접속 요청
 	client := network.NewClient()
-	err = client.Connect("tcp", "127.0.0.1:10011")
+	err = client.Connect("tcp", "13.124.76.58:20202")
 	if err != nil {
 		fmt.Println(err)
 		return
